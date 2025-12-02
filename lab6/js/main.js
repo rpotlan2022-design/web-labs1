b14de2b88fdcef000931ef2f9b39f34fe2466ddc
Roman <rpotlan2022@itcollege.lviv.ua>
2025-12-02 23:09:15 +0200

    Add lab6 with index.html and main.js with console results

---
+++ lab6/js/main.js
@@ -0,0 +1,7 @@
+
+console.log("Hello, Lab6!"); // В консолі з’явиться: Hello, Lab6!
+
+
+const header = document.querySelector("h1");
+console.log("Текст заголовка:", header.textContent); // Результат у консолі: Текст заголовка: Lab6 - JavaScript Test
+і
